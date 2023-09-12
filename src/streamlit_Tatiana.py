34144d0e140469f0cac9442a2eccb0f442719161
Your NameTatiana <dr.tatiana.keller@gmail.com>
2023-09-13 00:11:59 +0200

    show_source_button

--- src/streamlit_Tatiana.py
+++ src/streamlit_Tatiana.py
@@ -102,17 +102,21 @@ else:
 
 df_location_filtering = df_location[df_location['production'] >= 0]
 
+# Widgets: radio buttons
+show_sources = right_column.radio(
+    label='Show Sources Position', options=['Yes', 'No'])
 
 #Production of energy per canton
 fig_source = px.choropleth_mapbox(df_canton_prod, geojson=geojson, featureidkey='properties.kan_name', title=f"Total {source} Energy Production per Canton",
-                           locations=df_canton_prod.index, color="production", height=500)
+                           locations=df_canton_prod.index, color="production", height=500, opacity=0.7)
 
 fig_source.update_layout(mapbox_style="carto-positron",
                  mapbox_zoom=6, mapbox_center = {"lat": 46.8182, "lon": 8.2275})
 fig_source.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
 #fig.update_layout(title_text = "Total Hydro Energy Production per Canton")
-fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company"]).data[0])
-fig_source.update_traces(marker=dict(color='yellow', opacity=0.7),
+if show_sources == "Yes":
+    fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company"]).data[0])
+    fig_source.update_traces(marker=dict(color='yellow', opacity=0.7),
                   selector=dict(mode='markers'))
 
 st.plotly_chart(fig_source)  
