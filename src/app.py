88ee156ab9dc67a84cfdd3280150f210cbf1d40e
Your NameTatiana <dr.tatiana.keller@gmail.com>
2023-09-12 12:25:41 +0200

    live coding example added

--- src/app.py
+++ src/app.py
@@ -7,6 +7,14 @@ from urllib.request import urlopen
 import json
 from copy import deepcopy
 
+#put into cash
+@st.cache_data
+def load_data(path):
+    df=pd.read_csv(path)
+    return df
+mpg_df_raw=load_data(path='./data/mpg.csv')
+mpg_df=deepcopy(mpg_df_raw)
+
 mpg_df=pd.read_csv('./data/mpg.csv')
 st.title('Introduction to Streamlit')
 
