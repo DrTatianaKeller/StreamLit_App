f47c2f99a03d259a77b48377e3ae6fdd9a6520d1
Your NameTatiana <dr.tatiana.keller@gmail.com>
2023-09-13 00:18:21 +0200

    energy_source_level_2 added to info

--- src/streamlit_Tatiana.py
+++ src/streamlit_Tatiana.py
@@ -107,6 +107,7 @@ show_sources = right_column.radio(
     label='Show Sources Position', options=['Yes', 'No'])
 
 #Production of energy per canton
+
 fig_source = px.choropleth_mapbox(df_canton_prod, geojson=geojson, featureidkey='properties.kan_name', title=f"Total {source} Energy Production per Canton",
                            locations=df_canton_prod.index, color="production", height=500, opacity=0.7)
 
@@ -115,10 +116,15 @@ fig_source.update_layout(mapbox_style="carto-positron",
 fig_source.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
 #fig.update_layout(title_text = "Total Hydro Energy Production per Canton")
 if show_sources == "Yes":
-    fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company"]).data[0])
-    fig_source.update_traces(marker=dict(color='yellow', opacity=0.7),
+    if source == "All":
+        fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company",'energy_source_level_2']).data[0])
+        fig_source.update_traces(marker=dict(color='yellow', opacity=0.7),
                   selector=dict(mode='markers'))
-
+    else:
+        fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company"]).data[0])
+        fig_source.update_traces(marker=dict(color='energy_source_level_2', opacity=0.7),
+                  selector=dict(mode='markers'))
+        
 st.plotly_chart(fig_source)  
 
 
