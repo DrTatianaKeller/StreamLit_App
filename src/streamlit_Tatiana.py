f52549694c746a647c5af4e3ccfaef625faff160
Your NameTatiana <dr.tatiana.keller@gmail.com>
2023-09-13 00:30:15 +0200

    energy_source_level_2 color delete all modification

--- src/streamlit_Tatiana.py
+++ src/streamlit_Tatiana.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 from urllib.request import urlopen
 import json
 from copy import deepcopy
-from scicolorscales import *
+
 
 # First some MPG Data Exploration
 @st.cache_data
@@ -117,7 +117,7 @@ fig_source.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
 #fig.update_layout(title_text = "Total Hydro Energy Production per Canton")
 if show_sources == "Yes":
     if source == "All":
-        fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company","energy_source_level_2"]).data[0])
+        fig_source.add_trace(px.scatter_mapbox(df_location_filtering, lat='lat', lon='lon', size='production', hover_name="municipality", hover_data=["canton","production", "project_name","company",'energy_source_level_2']).data[0])
         fig_source.update_traces(marker=dict(color='yellow', opacity=0.7),
                   selector=dict(mode='markers'))
     else:
@@ -126,4 +126,7 @@ if show_sources == "Yes":
                   selector=dict(mode='markers'))
         
 st.plotly_chart(fig_source)  
+
+
+
 
